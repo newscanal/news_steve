cfd4748348f9944f9c1ffe7e1d90ddaab0a21c84
Eumgill98 <wjdghcks02@gmail.com>
2025-11-30 18:46:58 +0900

    feat(container): Add NewsContainer CRUD
    - Implemented NewsContainer CRUD
    	- Added `add` method that can multiple or a NewsItem add in
    	  NewsContainer
    	- Added `remove` method that can multiple or a NewsItem remove
    	  in NewsContainer
    	- Added `get` method that can multiple or a NewsItem get in
    	  NewsContainer
    	- If category, date, order is None not tracking index in
    	  NewsContainer

--- news_steve/container/news_container.py
+++ news_steve/container/news_container.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, DefaultDict
+from typing import Dict, List, Set, DefaultDict, Union
 from uuid import UUID
 from collections import defaultdict
 from sortedcontainers import SortedDict, SortedSet
@@ -21,12 +21,12 @@ class NewsContainer:
 
         self._index_by_category: DefaultDict[str, SortedSet[UUID]] = defaultdict(lambda: SortedSet(key=lambda uid: self._container[uid].published))
         self._index_by_order: DefaultDict[str, SortedSet[UUID]] = defaultdict(lambda: SortedSet(key=lambda uid: self._container[uid].published))
-        self._index_by_date: SortedDict[str, UUID] = SortedDict()
+        self._index_by_date: SortedDict[str, SortedSet[UUID]] = SortedDict()
         
     def _update_meta(self):
         self._meta['total_items'] = len(self)
-        self._meta['num_of_items_per_category'] = {k: len(v) for k,v in self._index_by_category.items()}
-        self._meta['num_of_items_per_order'] = {k: len(v) for k,v in self._index_by_order.items()}
+        self._meta['per_category'] = {k: len(v) for k,v in self._index_by_category.items()}
+        self._meta['per_order'] = {k: len(v) for k,v in self._index_by_order.items()}
 
     @property
     def categories(self) -> List[str]:
@@ -42,3 +42,87 @@ class NewsContainer:
     
     def __len__(self):
         return len(self._container)
+    
+    def _add(self, item: NewsItem):
+        if not isinstance(item, NewsItem):
+            raise ValueError(f"NewsContainer can only add NewsItems. This input is {type(item)}.")
+        id = item.item_id
+        self._container[id] = item
+
+        category = "None" if item.category is None else item.category
+        order = "None" if item.news_order is None else item.news_order.order_id
+        date = "None" if item.published is None else item.published
+
+        if category != "None":
+            self._index_by_category[category].add(id)
+
+        if order != "None":
+            self._index_by_order[order].add(id)
+        
+        if date != "None":
+            if date not in self._index_by_date:
+                self._index_by_date[date] = SortedSet()
+            self._index_by_date[date].add(id)       
+        
+    @meta_updater
+    def add(self, items: Union[NewsItem, List[NewsItem]]):
+        if isinstance(items, NewsItem):
+            items = [items]
+
+        for item in items:
+            self._add(item)
+
+    def _remove(self, item: NewsItem):
+        if not isinstance(item, NewsItem):
+            raise ValueError(f"NewsContainer can only remove NewsItems. This input is {type(item)}")
+                
+        id = item.item_id
+        if id not in self._container:
+            return
+        
+        category = "None" if item.category is None else item.category
+        order = "None" if item.news_order is None else item.news_order.order_id
+        date = "None" if item.published is None else item.published
+
+        if category != "None" and category in self._index_by_category:
+            self._index_by_category[category].discard(id)
+            if not self._index_by_category[category]:
+                del self._index_by_category[category]
+
+        if order != "None" and order in self._index_by_order:
+            self._index_by_order[order].discard(id)
+            if not self._index_by_order[order]:
+                del self._index_by_order[order]
+
+        if date != "None" and date in self._index_by_date:
+            self._index_by_date[date].discard(id)
+            if not self._index_by_date[date]:
+                del self._index_by_date[date]
+
+        del self._container[id]
+    
+    @meta_updater
+    def remove(self, items: Union[NewsItem, List[NewsItem]]):
+        if isinstance(items, NewsItem):
+            items = [items]
+
+        for item in items:
+            self._remove(item)        
+
+    def _get(self, key: UUID) -> NewsItem:
+        if key not in self._container:
+            raise KeyError(f"NewsItem with id {key} not found in NewsContainer.")
+        return self._container[key]
+    
+    def get(self, key: Union[str, UUID, List[UUID]]) -> List[NewsItem]:
+        single = False
+        if isinstance(key, (UUID, str)):
+            key = [UUID(key) if isinstance(key, str) else key]
+            single=True
+
+        result = [self._get(k) for k in key]
+        return result[0] if single else result
+
+    ## TODO Search
+
+    ## TODO Serialization/Deserialization

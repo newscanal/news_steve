35a7a634ddbebbd9a9cb50e1f0ca719f2df25255
Eumgill98 <wjdghcks02@gmail.com>
2025-12-01 19:13:43 +0900

    feat(container): Add NewsContainer search methods
    - Implemented NewsContainer search methods
    	- Added `search_by_category` that search NewsItems by a category
    	  or multiple categories.
    	- Added `search_by_order` that search NewsItems by a order or
    	  multiple orders.
    	- Added `search_by_date` that search NewsItems by start time and
    	  end time.
    	- Optional all methods `sort` and `reverse` factor.

--- news_steve/container/news_container.py
+++ news_steve/container/news_container.py
@@ -1,8 +1,9 @@
-from typing import Dict, List, Set, DefaultDict, Union
+from typing import Dict, List, Set, Optional, DefaultDict, Union
 from uuid import UUID
 from collections import defaultdict
 from sortedcontainers import SortedDict, SortedSet
 from news_steve.item import NewsItem
+from news_steve.order import NewsOrder
 
 def meta_updater(func):
     def wrapper(self, *args, **kwargs):
@@ -19,8 +20,8 @@ class NewsContainer:
         self._container: Dict[UUID, NewsItem] = dict()
         self._meta: Dict = dict()
 
-        self._index_by_category: DefaultDict[str, SortedSet[UUID]] = defaultdict(lambda: SortedSet(key=lambda uid: self._container[uid].published))
-        self._index_by_order: DefaultDict[str, SortedSet[UUID]] = defaultdict(lambda: SortedSet(key=lambda uid: self._container[uid].published))
+        self._index_by_category: DefaultDict[str, set[UUID]] = defaultdict(set)
+        self._index_by_order: DefaultDict[str, set[UUID]] = defaultdict(set)
         self._index_by_date: SortedDict[str, SortedSet[UUID]] = SortedDict()
         
     def _update_meta(self):
@@ -39,6 +40,11 @@ class NewsContainer:
     @property
     def meta(self) -> Dict[str, int]:
         return self._meta
+    
+    def get_all_itmes(self, sort: bool = False, revsere: bool = False):
+        if (sort):
+            return sorted(self._container.values(), key=lambda x:x.published, reverse=reversed)
+        return self._container.values()
     
     def __len__(self):
         return len(self._container)
@@ -66,6 +72,9 @@ class NewsContainer:
         
     @meta_updater
     def add(self, items: Union[NewsItem, List[NewsItem]]):
+        """
+        Add method that process a NewsItem or multiple NewsItmes.
+        """
         if isinstance(items, NewsItem):
             items = [items]
 
@@ -103,6 +112,9 @@ class NewsContainer:
     
     @meta_updater
     def remove(self, items: Union[NewsItem, List[NewsItem]]):
+        """
+        Remove method that process a NewsItem or multiple NewsItmes.
+        """
         if isinstance(items, NewsItem):
             items = [items]
 
@@ -115,6 +127,9 @@ class NewsContainer:
         return self._container[key]
     
     def get(self, key: Union[str, UUID, List[UUID]]) -> List[NewsItem]:
+        """
+        Get method that process a NewsItem or multiple NewsItmes.
+        """
         single = False
         if isinstance(key, (UUID, str)):
             key = [UUID(key) if isinstance(key, str) else key]
@@ -122,7 +137,73 @@ class NewsContainer:
 
         result = [self._get(k) for k in key]
         return result[0] if single else result
+    
+    def _search_category(self, category: str) -> Set[UUID]:
+        if category not in self._index_by_category:
+            raise ValueError(f"This category not in container. not valid category: {category}.")
+        return self._index_by_category[category]
 
-    ## TODO Search
+    def search_by_category(self, categories: Union[str, List[str]], sort=False, reverse=False) -> List[NewsItem]:
+        """
+        Search NewsItems by a category or multiple category
+        """
+        if isinstance(categories, str):
+            categories = [categories]
+
+        id_set = set()
+        for category in categories:
+            id_set.update(self._search_category(category))
+
+        result = [self._container[id_] for id_ in id_set]
+
+        if sort:
+            return sorted(result, key=lambda x: x.published, reverse=reverse)
+        return result
+
+
+    def _search_order(self, order:str) -> Set[UUID]:
+        if order not in self._index_by_order:
+            raise ValueError(f"This order not in container. not valid order: {order}.")
+        return self._index_by_order[order]
+
+    def serach_by_order(self, orders: Union[str, NewsOrder, List[str], List[NewsOrder]], sort=False, reverse=False) -> List[NewsItem]:
+        """
+        Search NewsItems by a NewsOrder or multiple NewsOrder
+        """
+        if isinstance(orders, (str, NewsOrder)):
+            orders = [orders]
+
+        id_set = set()
+        for order in orders:
+            if isinstance(order, NewsOrder):
+                key = order.order_id
+            else:
+                key = order
+            id_set.update(self._search_order(key))
+        
+        result = [self._container[id_] for id_ in id_set]
+
+        if sort:
+            return sorted(result, key=lambda x: x.published, reverse=reverse)
+        return result
+
+    def serach_by_date(self, start: str, end: Optional[str] = None, reverse=False):
+        """
+        Search NewsItems by start time and end time.
+        if end time is None, only start time NewsItems search.
+        """
+        if end is None:
+            end = start
+
+        keys = self._index_by_date.keys()
+
+        result = []
+        for date in keys.irange(start, end):
+            for id_ in self._index_by_date[date]:
+                result.append(self._container[id_])
+
+        if reverse:
+            return result.reverse()
+        return result
 
     ## TODO Serialization/Deserialization
